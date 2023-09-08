e68f46729fa4bad7314488bcb806e13f0c53ee85
Guillaume Caya-Letourneau <guillaume.caya@gsoft.com>
2023-09-08 15:25:04 -0400

    Fix tracing problem

--- src/Workleap.Extensions.MediatR/RequestTracingBehavior.cs
+++ src/Workleap.Extensions.MediatR/RequestTracingBehavior.cs
@@ -8,27 +8,24 @@ internal sealed class RequestTracingBehavior<TRequest, TResponse> : IPipelineBeh
 {
     public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
-        var activity = TracingHelper.StartActivity();
+        using var activity = TracingHelper.StartActivity();
         return activity == null ? next() : HandleWithTracing(request, next, activity);
     }
 
     private static async Task<TResponse> HandleWithTracing(TRequest request, RequestHandlerDelegate<TResponse> next, Activity activity)
     {
-        using (activity)
-        {
-            activity.DisplayName = request.GetType().Name;
+        activity.DisplayName = request.GetType().Name;
 
-            try
-            {
-                var result = await next().ConfigureAwait(false);
-                TracingHelper.MarkAsSuccessful(activity);
-                return result;
-            }
-            catch (Exception ex)
-            {
-                TracingHelper.MarkAsFailed(activity, ex);
-                throw;
-            }
+        try
+        {
+            var result = await next().ConfigureAwait(false);
+            TracingHelper.MarkAsSuccessful(activity);
+            return result;
+        }
+        catch (Exception ex)
+        {
+            TracingHelper.MarkAsFailed(activity, ex);
+            throw;
         }
     }
 }
