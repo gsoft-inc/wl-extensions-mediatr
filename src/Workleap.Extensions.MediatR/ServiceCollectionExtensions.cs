9210072f6144c00a6b0e3598bddda65b313cb1b5
Gérald Barré <meziantou@users.noreply.github.com>
2024-08-16 14:37:02 -0400

    Use codingstandards (#77)

--- src/Workleap.Extensions.MediatR/ServiceCollectionExtensions.cs
+++ src/Workleap.Extensions.MediatR/ServiceCollectionExtensions.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using Workleap.Extensions.MediatR;
 
-// ReSharper disable once CheckNamespace
+#pragma warning disable IDE0130 // Namespace does not match folder structure
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ServiceCollectionExtensions
